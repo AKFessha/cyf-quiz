625e7e73ec6c61211d5a4adc1952f20df48d31ae
Awet Fessha <awetkifle94@gmail.com>
2018-05-20 01:41:05 +0100

    changed the API to film questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,53 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Question from './Question';
+import Question from "./Question";
 
 class Questions extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       answers: [...Array(15)],
-      questions: [],
-    }
-    fetch('https://opentdb.com/api.php?amount=15&category=9&type=boolean')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      this.setState({
-        questions: data.results,
+      questions: []
+    };
+    fetch("https://opentdb.com/api.php?amount=15&category=11&type=boolean")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ questions: data.results });
       });
-    })
   }
 
   selectAnswer = (answer, index) => {
-
     const answersCopy = this.state.answers.slice();
     const correctAnswer = this.state.questions[index].correct_answer;
     answersCopy[index] = answer === correctAnswer;
 
-    this.setState({answers: answersCopy});
-  }
+    this.setState({ answers: answersCopy });
+  };
 
   renderQuestion = (questionItem, index) => {
     return (
-      <Question 
-        key={index} 
-        index={index} 
-        question={questionItem.question} 
+      <Question
+        key={index}
+        index={index}
+        question={questionItem.question}
         correctAnswer={questionItem.correct_answer}
-        selectAnswer={this.selectAnswer} 
-        isCorrect={this.state.answers[index]}/>
+        selectAnswer={this.selectAnswer}
+        isCorrect={this.state.answers[index]}
+      />
     );
-  }
-
+  };
 
   render() {
-
     return (
       <div>
-        { this.state.questions.map((questionItem, index) => this.renderQuestion(questionItem, index)) }
+        {this.state.questions.map((questionItem, index) =>
+          this.renderQuestion(questionItem, index)
+        )}
       </div>
     );
   }
